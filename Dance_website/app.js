bb742a933e5f0c7edf984f48633bac898d21bcc5
contributorsambhav <Sambhav511974@gmail.com>
2023-07-30 14:39:42 +0530

    Added more fields to form

--- Dance_website/app.js
+++ Dance_website/app.js
@@ -3,6 +3,24 @@ const app = express();
 const port = 80;
 const path = require('path')
 const fs = require('fs')
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser")
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/contactdetails');
+    console.log("Connected to database")
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const contactschema = new mongoose.Schema({
+    name: String
+
+});
+
+const contact = mongoose.model('contact', contactschema);
 
 
 
@@ -53,9 +71,12 @@ app.post("/",((req,res)=>{
 
     let objname = req.body.name
     let objage = req.body.age
+    let objphone = req.body.phone
+    let objemail = req.body.email
+    let objaddress=req.body.address
     let objgender = req.body.gender
     let objmore = req.body.more
-    let writeOutput = `The name of client is ${objname} \n Age is  ${objage}\n Gender is ${objgender} \n Some more info is that  \" ${objmore} \" \n \n `
+    let writeOutput = `The name of client is ${objname} \n Age is  ${objage}\n Gender is ${objgender} \n Some more info is that  \" ${objmore} \" \n Some contact details are \n Phone no. : ${objphone} \n Email : ${objemail} \n Address : ${objaddress} \n \n`
 
     fs.appendFileSync('Registrations.txt',writeOutput)
 
