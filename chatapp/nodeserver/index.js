99a74de4f79d7c0d2db048ee30691f72e6050559
contributorsambhav <Sambhav511974@gmail.com>
2023-09-02 19:17:59 +0530

    join and send requests handeled

--- chatapp/nodeserver/index.js
+++ chatapp/nodeserver/index.js
@@ -5,7 +5,13 @@ const io = require("socket.io")(8000)
 const users = {}
 
 io.on('connection',socket=>{
-    socket.on('user-joined',name=>{
-        
-    })
-})
+    socket.on('new-user-joined',name=>{
+        user[socket.id]=name;
+        socket.broadcast.emit('user-joined',name)
+    });
+
+    socket.on('send',message=>{
+        socket.broadcast.emit('recieve',{message:message,name:user[socket.id]});
+
+    });
+});
