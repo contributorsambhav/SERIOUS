2920d11c4f78d4fd0d3723e71d44b6582885022e
contributorsambhav <Sambhav511974@gmail.com>
2023-09-04 14:30:02 +0530

    client site made

--- chatapp/nodeserver/index.js
+++ chatapp/nodeserver/index.js
@@ -6,6 +6,7 @@ const users = {}
 
 io.on('connection',socket=>{
     socket.on('new-user-joined',name=>{
+        console.log("New user",name)
         user[socket.id]=name;
         socket.broadcast.emit('user-joined',name)
     });
