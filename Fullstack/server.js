1ad5e1386d6fb2bc415f957d4650e12c95ccc4cd
contributorsambhav <Sambhav511974@gmail.com>
2023-11-07 23:45:37 +0530

    pug engine applied and CSS structure destroyed

---
+++ Fullstack/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express();
+const port = 8000;
+const path = require('path')
+const fs = require('fs')
+
+
+
+// for serving static files
+app.use("/static",express.static("static"))
+
+app.use(express.urlencoded())
+
+
+// set the template engine as pug
+app.set('view engine','pug')
+
+//setting the views dir
+app.set('views',path.join(__dirname,'views'))
+
+
+app.get("/",((req,res)=>{
+    res.status(200).render("index.pug")
+}))
+
+
+app.listen(port,(()=>{
+    console.log("The application started succesfully on port "+port)
+}))
