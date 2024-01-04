878c5ca1f464a9fccda798c869ef169864767461
contributorsambhav <sambhav511974@gmail.com>
2024-01-04 12:16:47 +0530

    removed old files

--- chatapp/js/client.js
+++
@@ -1,8 +0,0 @@
-const socket = io("http://localhost:8000")
-
-const form = document.getElementById("sendcontainer")
-const messageInp = document.getElementById("messageInp")
-const messageContainer = document.querySelector(".container")
-
-const name = prompt("Enter your name to join: ")
-socket.emit("new-user-joined",name)
